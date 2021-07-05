63aa140291b6eec538e45b69415385b8ccf6b07b
Leo <takib77@gmail.com>
2021-07-05 15:46:54 +0200

    task-03 kész

--- task-03/index.js
+++ task-03/index.js
@@ -2,19 +2,32 @@
  * 1. Töltsd be a json állományok kezelését végző modult az azonos mappából és 
  * emeld ki egy változóba az update metódusát.
  */
-
+const { update } = require('./jsonHandler')
 /**
- * 2. Ebben a névtelen aszinkron függvényben futtatsd az update metódust. 
- * Ha készen van a json kezelő modulod, akkor csupán élesítened kell az alábbi 
+ * 2. Ebben a névtelen aszinkron függvényben futtatsd az update metódust.
+ * Ha készen van a json kezelő modulod, akkor csupán élesítened kell az alábbi
  * kódot a komment jelek eltávolításával.
  */
-/*
-( async () => {
-    console.log( await update({
-        "id": 7,
-        "name": "Wine - Badacsony - Hungarian",
-        "price": 99,
-        "bestBefore": "2021-05-30"
-    }));
-})();
-*/
+
+const updater = async (entity) => {
+    await update(entity);
+}
+
+updater({
+    "id": 7,
+    "name": "Wine - Badacsony - Hungarian",
+    "price": 99,
+    "bestBefore": "2021-05-30"
+})
+
+// Az eredeti teszkóddal TypeErrort kaptam.
+
+// (async () => {
+//     console.log(await update({
+//         "id": 7,
+//         "name": "Wine - Badacsony - Hungarian",
+//         "price": 99,
+//         "bestBefore": "2021-05-30"
+//     }));
+// })();
+
--- task-03/jsonHandler.js
+++ task-03/jsonHandler.js
@@ -1,21 +1,23 @@
 /**
  * 1. A fájlok kezeléséhez az fs modul promise alapú verzióját használd.
  */
-
+const fsp = require('fs').promises
+const { join } = require('path')
 /**
  * 2. Állítsd be az azonos mappában található .json fájl elérési útját a path 
  * modul join metódusának segítségével.
  */
- const jsonPath = '';
+const jsonPath = join(__dirname, 'db', 'products.json');
 
- /**
-  * 3. A jsonPath útvonalon található fájl tartalmát beolvassa és értelmezi, 
-  * majd visszaadja a kapott tömböt.
-  * @returns objektumok tömbje
-  */
- const getList = async () => {
-     //
- };
+/**
+ * 3. A jsonPath útvonalon található fájl tartalmát beolvassa és értelmezi, 
+ * majd visszaadja a kapott tömböt.
+ * @returns objektumok tömbje
+ */
+const getList = async () => {
+    const list = await fsp.readFile(jsonPath, 'utf8')
+    return JSON.parse(list)
+};
 
 /**
  * 4. A kapott tömböt json formátumra alakítja és beleírja a jsonPath útvonalon 
@@ -24,7 +26,8 @@
  * @returns 
  */
 const saveList = async (list = []) => {
-    //
+    const newList = JSON.stringify(list, null, 4)
+    await fsp.writeFile(jsonPath, newList)
 };
 
 /**
@@ -35,9 +38,14 @@ const saveList = async (list = []) => {
  * @returns a frissített objektum ha sikerült a frissítés, egyébként false
  */
 const update = async (entity = {}) => {
-    //
+    const list = await getList();
+    const entityIndex = list.findIndex(item => item.id === entity.id);
+    list[entityIndex] = { ...list[entityIndex], ...entity };
+    await saveList(list);
 };
 
 module.exports = {
     update,
+    getList,
+    saveList
 };
