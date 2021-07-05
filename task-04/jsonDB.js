77e8c2a40926f2c32d58da88d245660ec601307f
Leo <takib77@gmail.com>
2021-07-05 15:53:13 +0200

    task-04 kész

--- task-04/jsonDB.js
+++ task-04/jsonDB.js
@@ -1,12 +1,13 @@
 /**
  * 1. A fájlok kezeléséhez az fs modul promise alapú verzióját használd.
  */
-
+const fsp = require('fs').promises
+const { join } = require('path')
 /**
- * 2. Állítsd be az azonos mappában található .json fájl elérési útját a path 
- * modul join metódusának segítségével.
- */
-const jsonPath = '';
+* 2. Állítsd be az azonos mappában található .json fájl elérési útját a path 
+* modul join metódusának segítségével.
+*/
+const jsonPath = join(__dirname, 'db', 'products.json');
 
 /**
  * 3. A jsonPath útvonalon található fájl tartalmát beolvassa és értelmezi, 
@@ -14,7 +15,8 @@ const jsonPath = '';
  * @returns objektumok tömbje
  */
 const getList = async () => {
-    //
+    const list = await fsp.readFile(jsonPath, 'utf8')
+    return JSON.parse(list)
 };
 
 /**
@@ -23,8 +25,9 @@ const getList = async () => {
  * 
  * @returns a termékek objektumai egy tömbben
  */
- const getAll = async () => {
-     //
+const getAll = async () => {
+    const list = await getList()
+    return list
 };
 
 module.exports = {
--- task-04/server.js
+++ task-04/server.js
@@ -2,16 +2,25 @@
  * 1. Töltsd be a szükséges modulokat a http kérésekhez és a jsonDB getAll 
  * függvényét.
  */
-
+const { getAll } = require('./jsonDB');
+const http = require('http');
 // 2. Definiáld a port értékét 8080 -ra a port változóban.
-
+const port = 8080
 /**
  * 3. Hozz létre egy http szervert.
  * A szerver get kérés esetén a getAll függvény segítségével lekéri a listát, 
  * majd beállítja a headert, hogy json a tartalom típusa, 
  * végül visszaküldi a kliensnek a listában tárolt adatokat.
  */
+const server = http.createServer(async (req, res) => {
+    const list = await getAll();
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(list));
+})
 
 /**
  * 4. Állítsd be, hogy a szerver figyelje a port változóban definiált portot.
- */ 
+ */
+server.listen(port, () => {
+    console.log(`Server is running: http://127.0.0.1:${port}`);
+})
